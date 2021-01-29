c55450bb7996a7de0ed5fa1865983e5e2d7c908f
dmitrysib <skydim@gmail.com>
2021-01-29 10:15:31 +0100

    HW9.11

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -8,9 +8,12 @@ public class MainFile {
     public static void recursiveDirScan(String dir, int level) {
         try {
             Files.list(Paths.get(dir)).forEach(path -> {
-                System.out.println(" ".repeat(level * 2) + path.toString());
+                var fn = " ".repeat(level * 2) + path.toString().replace(dir + '/', "");
                 if (Files.isDirectory(path)) {
+                    System.out.println(fn + " -->");
                     recursiveDirScan(path.toString(), level + 1);
+                } else {
+                    System.out.println(fn);
                 }
             });
         } catch (IOException e) {
