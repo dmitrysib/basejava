b2c29a9c80205722c2ff3b1282f364d0f8c6b828
dmitrysib <skydim@gmail.com>
2021-02-01 21:31:04 +0100

    HW10.3 помоему увлекся

--- src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
+++ src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
@@ -14,21 +14,21 @@ public class DataStreamSerializer implements Serializer {
             dos.writeUTF(resume.getUuid());
             dos.writeUTF(resume.getFullName());
 
-            doForEach(dos, resume.getContacts().entrySet(), contact -> {
+            doForEachOut(dos, resume.getContacts().entrySet(), contact -> {
                 dos.writeUTF(contact.getKey().name());
                 dos.writeUTF(contact.getValue());
             });
 
-            doForEach(dos, resume.getSections().entrySet(), section -> {
+            doForEachOut(dos, resume.getSections().entrySet(), section -> {
                 dos.writeUTF(section.getKey().name());
                 switch (section.getKey()) {
                     case PERSONAL, OBJECTIVE -> dos.writeUTF(((StringSection) section.getValue()).getValue());
-                    case ACHIEVEMENT, QUALIFICATIONS -> doForEach(dos, ((ListSection) section.getValue()).getElements(), dos::writeUTF);
-                    case EDUCATION, EXPERIENCE -> doForEach(dos, ((Organization) section.getValue()).getElements(), experience -> {
+                    case ACHIEVEMENT, QUALIFICATIONS -> doForEachOut(dos, ((ListSection) section.getValue()).getElements(), dos::writeUTF);
+                    case EDUCATION, EXPERIENCE -> doForEachOut(dos, ((Organization) section.getValue()).getElements(), experience -> {
                         dos.writeUTF(experience.getHomePage().getTitle());
                         dos.writeUTF(experience.getHomePage().getUrl());
 
-                        doForEach(dos, experience.getPositions(), position -> {
+                        doForEachOut(dos, experience.getPositions(), position -> {
                             dos.writeUTF(position.getTitle());
                             dos.writeUTF(position.getStartDate());
                             dos.writeUTF(position.getEndDate());
@@ -45,55 +45,73 @@ public class DataStreamSerializer implements Serializer {
         try (var dis = new DataInputStream(is)) {
             Resume resume = new Resume(dis.readUTF(), dis.readUTF());
 
-            int size = dis.readInt();
-            for (int i = 0; i < size; i++) {
-                resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
-            }
+            doForEachIn(dis, () -> resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
 
-            int sectionsCount = dis.readInt();
-            for (int i = 0; i < sectionsCount; i++) {
+            doForEachIn(dis, () -> {
                 SectionType sectionType = SectionType.valueOf(dis.readUTF());
                 switch (sectionType) {
                     case OBJECTIVE, PERSONAL -> resume.addSection(sectionType, new StringSection(dis.readUTF()));
-                    case ACHIEVEMENT, QUALIFICATIONS -> {
-                        int listSize = dis.readInt();
-                        List<String> stringList = new ArrayList<>();
-                        for (int k = 0; k < listSize; k++) {
-                            stringList.add(dis.readUTF());
-                        }
-                        resume.addSection(sectionType, new ListSection(stringList));
-                    }
-                    case EDUCATION, EXPERIENCE -> {
-                        int orgSize = dis.readInt();
-                        List<Experience> experiences = new ArrayList<>();
-                        for (int k = 0; k < orgSize; k++) {
-                            Experience e = new Experience(dis.readUTF(), dis.readUTF());
-
-                            int posSize = dis.readInt();
-                            List<Experience.Position> positions = new ArrayList<>();
-                            for (int m = 0; m < posSize; m++) {
-                                positions.add(new Experience.Position(dis.readUTF(), dis.readUTF(), dis.readUTF(), dis.readUTF()));
-                            }
-                            e.setPositions(positions);
-                            experiences.add(e);
-                        }
-                        resume.addSection(sectionType, new Organization(experiences));
-                    }
+                    case ACHIEVEMENT, QUALIFICATIONS -> resume.addSection(sectionType, new ListSection(readList(dis)));
+                    case EDUCATION, EXPERIENCE -> resume.addSection(sectionType, new Organization(readExperienceList(dis, e -> e.setPositions(readPosition(dis)))));
                 }
-            }
-
+            });
             return resume;
         }
     }
 
-    private interface Action<T> {
+    private <T> void doForEachOut(DataOutputStream dos, Collection<T> collection, ActionOut<T> actionOut) throws IOException {
+        dos.writeInt(collection.size());
+        for (T item : collection) {
+            actionOut.action(item);
+        }
+    }
+
+    private void doForEachIn(DataInputStream dis, ActionIn actionIn) throws IOException {
+        int size = dis.readInt();
+        for (int i = 0; i < size; i++) {
+            actionIn.action();
+        }
+    }
+
+    private List<String> readList(DataInputStream dis) throws IOException {
+        int size = dis.readInt();
+        List<String> result = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            result.add(dis.readUTF());
+        }
+        return result;
+    }
+
+    private List<Experience> readExperienceList(DataInputStream dis, ActionInExperience action) throws IOException {
+        int size = dis.readInt();
+        List<Experience> result = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            Experience e = new Experience(dis.readUTF(), dis.readUTF());
+            action.action(e);
+            result.add(e);
+        }
+        return result;
+    }
+
+    private List<Experience.Position> readPosition(DataInputStream dis) throws IOException {
+        int size = dis.readInt();
+        List<Experience.Position> result = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            Experience.Position position = new Experience.Position(dis.readUTF(), dis.readUTF(), dis.readUTF(), dis.readUTF());
+            result.add(position);
+        }
+        return result;
+    }
+
+    private interface ActionOut<T> {
         void action(T t) throws IOException;
     }
 
-    private <T> void doForEach(DataOutputStream dos, Collection<T> collection, Action<T> action) throws IOException {
-        dos.writeInt(collection.size());
-        for (T item : collection) {
-            action.action(item);
-        }
+    private interface ActionIn {
+        void action() throws IOException;
+    }
+
+    private interface ActionInExperience {
+        void action(Experience e) throws IOException;
     }
 }
