ffc1387167832bc21c673ab096caed248f08e6f2
dmitrysib <skydim@gmail.com>
2021-02-01 19:14:06 +0100

    hw10.1 намутилось с интерфейсами

--- src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
+++ src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
@@ -1,12 +1,12 @@
 package ru.javawebinar.basejava.storage.serializer;
 
-import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.*;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 public class DataStreamSerializer implements Serializer {
     @Override
@@ -15,41 +15,27 @@ public class DataStreamSerializer implements Serializer {
             dos.writeUTF(resume.getUuid());
             dos.writeUTF(resume.getFullName());
 
-            Map<ContactType, String> contacts = resume.getContacts();
-            dos.writeInt(contacts.size());
-            contacts.forEach((contactType, contact) -> {
-                writeUTF(contactType.name(), dos);
-                writeUTF(contact, dos);
+            execute(dos, resume.getContacts().entrySet(), contact -> {
+                dos.writeUTF(contact.getKey().name());
+                dos.writeUTF(contact.getValue());
             });
 
-            Map<SectionType, AbstractSection> sections = resume.getSections();
-            dos.writeInt(sections.size());
-            sections.forEach((sectionType, section) -> {
-                writeUTF(sectionType.name(), dos);
-                switch (sectionType) {
-                    case PERSONAL, OBJECTIVE -> writeUTF(((StringSection) section).getValue(), dos);
-                    case ACHIEVEMENT, QUALIFICATIONS -> {
-                        List<String> list = ((ListSection) section).getElements();
-                        writeInt(list.size(), dos);
-                        list.forEach(s -> writeUTF(s, dos));
-                    }
-                    case EDUCATION, EXPERIENCE -> {
-                        List<Experience> list = ((Organization) section).getElements();
-                        writeInt(list.size(), dos);
-                        list.forEach(e -> {
-                            writeUTF(e.getHomePage().getTitle(), dos);
-                            writeUTF(e.getHomePage().getUrl(), dos);
+            execute(dos, resume.getSections().entrySet(), section -> {
+                dos.writeUTF(section.getKey().name());
+                switch (section.getKey()) {
+                    case PERSONAL, OBJECTIVE -> dos.writeUTF(((StringSection) section.getValue()).getValue());
+                    case ACHIEVEMENT, QUALIFICATIONS -> execute(dos, ((ListSection) section.getValue()).getElements(), dos::writeUTF);
+                    case EDUCATION, EXPERIENCE -> execute(dos, ((Organization) section.getValue()).getElements(), experience -> {
+                        dos.writeUTF(experience.getHomePage().getTitle());
+                        dos.writeUTF(experience.getHomePage().getUrl());
 
-                            List<Experience.Position> positions = e.getPositions();
-                            writeInt(positions.size(), dos);
-                            positions.forEach(p -> {
-                                writeUTF(p.getTitle(), dos);
-                                writeUTF(p.getStartDate(), dos);
-                                writeUTF(p.getEndDate(), dos);
-                                writeUTF(p.getDescription(), dos);
-                            });
+                        execute(dos, experience.getPositions(), position -> {
+                            dos.writeUTF(position.getTitle());
+                            dos.writeUTF(position.getStartDate());
+                            dos.writeUTF(position.getEndDate());
+                            dos.writeUTF(position.getDescription());
                         });
-                    }
+                    });
                 }
             });
         }
@@ -78,7 +64,7 @@ public class DataStreamSerializer implements Serializer {
                         }
                         resume.addSection(sectionType, new ListSection(stringList));
                     }
-                    case EDUCATION,EXPERIENCE -> {
+                    case EDUCATION, EXPERIENCE -> {
                         int orgSize = dis.readInt();
                         List<Experience> experiences = new ArrayList<>();
                         for (int k = 0; k < orgSize; k++) {
@@ -101,19 +87,25 @@ public class DataStreamSerializer implements Serializer {
         }
     }
 
-    private void writeUTF(String str, DataOutputStream dos) {
-        try {
-            dos.writeUTF(str);
-        } catch (IOException e) {
-            throw new StorageException("Can't write to file", e);
-        }
+    /*
+    Определяется функциональный интерфейс, в котром описан метод принимающий один аргумент
+    любого типа
+     */
+    private interface FunctionalBlock<T> {
+        void lambda(T t) throws IOException;
     }
 
-    private void writeInt(int number, DataOutputStream dos) {
-        try {
-            dos.writeInt(number);
-        } catch (IOException e) {
-            throw new StorageException("Can't write to file", e);
+    /*
+    на вход обобщеного метода подается выходной поток, коллеция и лямбда
+    краткое описание - перебрать колекцию по элементам и что-то сделать с каждым элементом. Что сделать - описано в
+    функциональном блоке
+    Это прикольно но это работает типа
+    execute(dos, Arrays.asList("a", "b", "c"), System.out::println);
+     */
+    private <T> void execute(DataOutputStream dos, Collection<T> collection, FunctionalBlock<T> functionalBlock) throws IOException {
+        dos.writeInt(collection.size());
+        for (T item : collection) {
+            functionalBlock.lambda(item);
         }
     }
 }
