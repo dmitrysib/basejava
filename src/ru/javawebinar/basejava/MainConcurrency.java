42a7bac07eb5e75fb404708d49276cff0cbaa5e6
dmitrysib <skydim@gmail.com>
2021-02-02 23:17:26 +0100

    add sleep to deadlock

--- src/ru/javawebinar/basejava/MainConcurrency.java
+++ src/ru/javawebinar/basejava/MainConcurrency.java
@@ -1,53 +1,42 @@
 package ru.javawebinar.basejava;
 
-import java.sql.ResultSet;
-
 public class MainConcurrency {
+
+    static final Resource RESOURCE_1 = new Resource();
+    static final Resource RESOURCE_2 = new Resource();
 
     public static void main(String[] args) throws InterruptedException {
 
-//        Simple deadlock with one resource
-//        synchronized (MainConcurrency.class) {
-//            Thread thread = new Thread(() -> {
-//                System.out.println("Trying lock");
-//                synchronized (MainConcurrency.class) {
-//                    System.out.println("MainConcurrency locked!");
-//                }
-//            });
-//            thread.start();
-//            thread.join();
-//        }
+        Thread thread1 = new Thread(() -> {
+            synchronized (RESOURCE_1) {
+                System.out.println(Thread.currentThread().getName() + ": lock resource 1");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ignored) {
+                }
+                synchronized (RESOURCE_2) {
+                    System.out.println(Thread.currentThread().getName() + ": lock resource 2");
+                }
+            }
+        }, "Thread1");
 
-        Thread thread1 = new Thread(MainConcurrency::part1);
-        Thread thread2 = new Thread(MainConcurrency::part2);
+        Thread thread2 = new Thread(() -> {
+            synchronized (RESOURCE_2) {
+                System.out.println(Thread.currentThread().getName() + ": lock resource 2");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ignored) {
+                }
+                synchronized (RESOURCE_1) {
+                    System.out.println(Thread.currentThread().getName() + ": lock resource 1");
+                }
+            }
+        }, "Thread2");
 
         thread1.start();
         thread2.start();
     }
 
     static class Resource {
-        public int val = 1;
-    }
-    static final Resource RESOURCE_1 = new Resource();
-    static final Resource RESOURCE_2 = new Resource();
-
-    private static void part1() {
-        synchronized (RESOURCE_1) {
-            System.out.println("part1: lock resource 1");
-            try {Thread.sleep(100); } catch (InterruptedException e) { }
-            synchronized (RESOURCE_2) {
-                System.out.println("part1: lock resource 2");
-            }
-        }
-    }
-
-    private static void part2() {
-        synchronized (RESOURCE_2) {
-            System.out.println("part2: lock resource 2");
-            try {Thread.sleep(100); } catch (InterruptedException e) { }
-            synchronized (RESOURCE_1) {
-                System.out.println("part2: lock resource 1");
-            }
-        }
     }
 }
