4fda11588e97410e3bbd79592e295db887fc037e
dmitrysib <skydim@gmail.com>
2021-02-02 23:48:56 +0100

    refactoring

--- src/ru/javawebinar/basejava/MainConcurrency.java
+++ src/ru/javawebinar/basejava/MainConcurrency.java
@@ -3,8 +3,8 @@ package ru.javawebinar.basejava;
 public class MainConcurrency {
     public static void main(String[] args) {
 
-        Thread thread1 = new Thread(() -> doTask("ResourceA", "ResourceB"), "Thread1");
-        Thread thread2 = new Thread(() -> doTask("ResourceB", "ResourceA"), "Thread2");
+        Thread thread1 = new Thread(() -> doTask("A", "B"), "Thread1");
+        Thread thread2 = new Thread(() -> doTask("B", "A"), "Thread2");
 
         thread1.start();
         thread2.start();
@@ -14,14 +14,14 @@ public class MainConcurrency {
     private static void doTask(Object resource1,Object resource2) {
         synchronized (resource1) {
             String threadName = Thread.currentThread().getName();
-            System.out.println(threadName + ": lock resource " + resource1);
+            System.out.println(threadName + ": lock object " + resource1);
 
             DateUtil.sleep();
 
-            System.out.println(threadName + ": waiting resource  " + resource2);
+            System.out.println(threadName + ": waiting object " + resource2);
 
             synchronized (resource2) {
-                System.out.println(threadName + ": lock resource  " + resource2);
+                System.out.println(threadName + ": lock object " + resource2);
             }
         }
     }
