da670c9c0272f1fd8b1a82aa4dd698d1d3bc7fe0
dmitrysib <skydim@gmail.com>
2021-02-03 13:27:42 +0100

    final fix & refactoring

--- src/ru/javawebinar/basejava/MainConcurrency.java
+++ src/ru/javawebinar/basejava/MainConcurrency.java
@@ -3,11 +3,8 @@ package ru.javawebinar.basejava;
 public class MainConcurrency {
     public static void main(String[] args) {
 
-        Thread thread1 = new Thread(() -> doTask("A", "B"), "Thread1");
-        Thread thread2 = new Thread(() -> doTask("B", "A"), "Thread2");
-
-        thread1.start();
-        thread2.start();
+        new Thread(() -> doTask("A", "B"), "Thread1").start();
+        new Thread(() -> doTask("B", "A"), "Thread2").start();
     }
 
     @SuppressWarnings("SynchronizationOnLocalVariableOrMethodParameter")
@@ -16,21 +13,15 @@ public class MainConcurrency {
             String threadName = Thread.currentThread().getName();
             System.out.println(threadName + ": lock object " + object1);
 
-            TimeUtil.sleep();
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ignored) {
+            }
 
             System.out.println(threadName + ": waiting object " + object2);
 
             synchronized (object2) {
                 System.out.println(threadName + ": lock object " + object2);
-            }
-        }
-    }
-
-    static class TimeUtil {
-        static void sleep() {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException ignored) {
             }
         }
     }
