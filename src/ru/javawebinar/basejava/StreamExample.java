89082a8053f1239374e7a597522242079134dcf3
dmitrysib <skydim@gmail.com>
2021-02-04 21:42:27 +0100

    Lesson 12

--- src/ru/javawebinar/basejava/StreamExample.java
+++ src/ru/javawebinar/basejava/StreamExample.java
@@ -2,13 +2,16 @@ package ru.javawebinar.basejava;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class StreamExample {
 
     public static void main(String[] args) {
 
-        List<Integer> list = List.of(8, 9);
+        List<Integer> list = List.of(1, 2, 3, 4, 5);
         System.out.println(list + " --> oddOrEven --> " + oddOrEven(list));
 
         int[] array = {1, 2, 3, 3, 2, 3};
@@ -17,20 +20,21 @@ public class StreamExample {
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
 
-        var odd = new ArrayList<>(integers);
-        var even = new ArrayList<>(integers);
+//        var odd = new ArrayList<>(integers);
+//        var even = new ArrayList<>(integers);
 
-        int sum = integers.stream()
-                .peek(i -> {
-                    if (i % 2 == 0) {
-                        odd.remove(i);
-                    } else {
-                        even.remove(i);
-                    }
-                })
-                .reduce(Integer::sum).orElse(0);
+//        int sum = integers.stream()
+//                .peek(i -> {
+//                    if (i % 2 == 0) {
+//                        odd.remove(i);
+//                    } else {
+//                        even.remove(i);
+//                    }
+//                })
+//                .reduce(Integer::sum).orElse(0);
 
-        return sum % 2 == 0 ? odd : even;
+        int sum = integers.stream().reduce(Integer::sum).orElse(0);
+        return integers.stream().filter(i -> i % 2 != sum % 2).collect(Collectors.toList());
     }
 
     private static int minValue(int[] values) {
