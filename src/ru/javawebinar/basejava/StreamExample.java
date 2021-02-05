a452f03b7d90f1511daba4b269aa7e65bc7e2774
dmitrysib <skydim@gmail.com>
2021-02-05 10:34:37 +0100

    HW12 variant

--- src/ru/javawebinar/basejava/StreamExample.java
+++ src/ru/javawebinar/basejava/StreamExample.java
@@ -1,17 +1,14 @@
 package ru.javawebinar.basejava;
 
-import java.util.*;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.*;
 
 public class StreamExample {
 
     public static void main(String[] args) {
 
-        List<Integer> list = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> list = List.of(8, 9);
         System.out.println(list + " --> oddOrEven --> " + oddOrEven(list));
 
         int[] array = {1, 2, 3, 3, 2, 3};
@@ -19,11 +16,11 @@ public class StreamExample {
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-
-        var l = integers.parallelStream().collect(Collectors.groupingBy(i -> i % 2 == 0 ? "even" : "odd"));
-        System.out.println(l);
-
-        return null;
+        var result = integers
+                .parallelStream()
+                .collect(Collectors.groupingBy(i -> i % 2 == 0 ? "even" : "odd")
+                );
+        return result.get("odd").size() % 2 == 0 ? result.get("even") : result.get("odd");
     }
 
     private static int minValue(int[] values) {
