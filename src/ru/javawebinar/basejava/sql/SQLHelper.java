4350f5cefe1051a48355d5daae16e835079a414d
dmitrysib <skydim@gmail.com>
2021-02-06 18:04:43 +0100

    HW13.9 Реализация SQL

--- src/ru/javawebinar/basejava/sql/SQLHelper.java
+++ src/ru/javawebinar/basejava/sql/SQLHelper.java
@@ -1,10 +1,23 @@
 package ru.javawebinar.basejava.sql;
 
+import ru.javawebinar.basejava.exception.ExistStorageException;
 import ru.javawebinar.basejava.exception.StorageException;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
+class SQLExceptionHelper extends RuntimeException {
+    private static final String POSTGRES_DUPLICATE_KEY_STATEMENT = "23505";
+    private static final int MYSQL_DUPLICATE_KEY_CODE = 1062;
+
+    SQLExceptionHelper(SQLException e) {
+        if (POSTGRES_DUPLICATE_KEY_STATEMENT.equals(e.getSQLState()) || MYSQL_DUPLICATE_KEY_CODE == e.getErrorCode()) {
+            throw new ExistStorageException(null);
+        }
+        throw new StorageException(e);
+    }
+}
 
 public class SQLHelper {
     private final ConnectionFactory cf;
