ffe40e5384ef9fc9a95d5c51a318468c91e809f8
dmitrysib <skydim@gmail.com>
2021-02-09 16:13:23 +0100

    HW15.2

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -3,19 +3,37 @@ package ru.javawebinar.basejava.web;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import ru.javawebinar.basejava.Config;
+import ru.javawebinar.basejava.model.Resume;
+import ru.javawebinar.basejava.storage.Storage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
-
-        String name = request.getParameter("name");
-
         response.setHeader("Content-type", "text/html; charset=UTF-8");
-        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
+
+        Storage storage = Config.getInstance().getSqlStorage();
+        String uuid = request.getParameter("uuid");
+
+        List<Resume> resumes = uuid == null ? storage.getAllSorted() : new ArrayList<>(List.of(storage.get(uuid)));
+
+        response.getWriter().write("<table>");
+        resumes.forEach(resume -> {
+            try {
+                response.getWriter().write("<tr><td>" + resume.getUuid() + "</td><td>" + resume.getFullName() + "</td></tr>");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        response.getWriter().write("</table>");
+
     }
 
     @Override
