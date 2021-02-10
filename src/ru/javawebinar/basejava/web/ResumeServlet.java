d65efcefb142627143f1b19daf05a9b12cab08cf
dmitrysib <skydim@gmail.com>
2021-02-10 22:59:15 +0100

    HW15

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -1,13 +1,14 @@
 package ru.javawebinar.basejava.web;
 
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import ru.javawebinar.basejava.Config;
 import ru.javawebinar.basejava.model.Resume;
 import ru.javawebinar.basejava.storage.Storage;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
@@ -20,15 +21,23 @@ public class ResumeServlet extends HttpServlet {
 
         Storage storage = Config.getInstance().getSqlStorage();
         String uuid = request.getParameter("uuid");
-        List<Resume> resumes = uuid == null ? storage.getAllSorted() : List.of(storage.get(uuid));
+
+        List<Resume> resumes = uuid == null ? storage.getAllSorted() : new ArrayList<>(List.of(storage.get(uuid)));
+
         response.getWriter().write("<table>");
-        for (Resume resume : resumes) {
-            response.getWriter().write("<tr><td>" + resume.getUuid() + "</td><td>" + resume.getFullName() + "</td></tr>");
-        }
+        resumes.forEach(resume -> {
+            try {
+                response.getWriter().write("<tr><td>" + resume.getUuid() + "</td><td>" + resume.getFullName() + "</td></tr>");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
         response.getWriter().write("</table>");
+
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+
     }
 }
